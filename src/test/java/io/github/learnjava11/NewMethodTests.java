a927cf710721c318a2bee872d77f0b936194d1f6
Michael Puening <mpuening@gmail.com>
2020-09-22 20:31:48 -0400

    Added more tests for new methods in Java 11

---
+++ src/test/java/io/github/learnjava11/NewMethodTests.java
@@ -0,0 +1,33 @@
+package io.github.learnjava11;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.Test;
+
+public class NewMethodTests {
+
+	@Test
+	public void testToArrayMethod() {
+		Set<String> set = new TreeSet<String>(Set.of("A", "B", "C"));
+
+		String[] array = set.toArray(String[]::new);
+		assertEquals("A", array[0]);
+		assertEquals("B", array[1]);
+		assertEquals("C", array[2]);
+	}
+
+	@Test
+	public void testReadFileIntoStringAndParse() throws IOException {
+		String content = Files.readString(Path.of("src/test/resources/file.txt"));
+		content.lines().forEach(line -> {
+			assertTrue(line.startsWith("This is line"));
+		});
+	}
+}
